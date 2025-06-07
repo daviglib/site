d47478e8ee3cad3831c56ed1f3bdf191cf7e7dfa
Davi Garcia Liberato <daviglib@gmail.com>
2025-06-07 18:19:06 -0300

    final

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.ronfinigarcia.com.br/',
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+  outDir: './public',
+}
